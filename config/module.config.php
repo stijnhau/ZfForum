831b90157f61781f50f33fdd1bf2d8f73f1b104c
Stijn Haulotte <stijnhau@gmail.com>
2014-07-23 21:43:10 +0200

    Added reply option in serparate view

--- config/module.config.php
+++ config/module.config.php
@@ -42,7 +42,7 @@ return array(
             'Zf2Forum' => array(
                 'type'    => 'Zend\Mvc\Router\Http\Segment',
                 'options' => array(
-                    'route'    => '/:tagslug{-}-:tagid',
+                    'route'    => '/:tagslug{-}-:tagid[/]',
                     'constraints' => array(
                         'tagslug' => '[a-zA-Z][a-zA-Z0-9_-]*',
                         'tagid'   => '[0-9]+',
@@ -57,7 +57,7 @@ return array(
                     'thread' => array(
                         'type'    => 'Zend\Mvc\Router\Http\Segment',
                         'options' => array(
-                            'route'    => '/:threadslug{-}-:threadid/:action',
+                            'route'    => ':threadslug{-}-:threadid[/]',
                             'constraints' => array(
                                 'threadslug' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'threadid'   => '[0-9]+',
@@ -68,11 +68,22 @@ return array(
                             ),
                         ),
                         'may_terminate' => true,
+                        'child_routes' => array(
+                            'newmessage' => array(
+                                'type' => 'Literal',
+                                'options' => array(
+                                    'route' => 'newmessage',
+                                    'defaults' => array(
+                                        'action' => 'newmessage'
+                                    )
+                                )
+                            )
+                        ),
                     ),
                     'newthread' => array(
                         'type'    => 'Zend\Mvc\Router\Http\Segment',
                         'options' => array(
-                            'route'    => '/newthread',
+                            'route'    => 'newthread',
                             'defaults' => array(
                                 'controller' => 'Zf2Forum\Controller\DiscussController',
                                 'action'     => 'newthread',
