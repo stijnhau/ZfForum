65dfcdd00929b8585e2d0fc8abfa7f4c2f9dc40f
Stijn Haulotte <stijnhau@gmail.com>
2015-09-06 18:43:47 +0200

    Setup test framework

---
+++ src/Zf2Forum/Form/replyForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Zf2Forum\Form;
+
+use Zend\Form\Form;
+
+class ReplyForm extends Form
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->add(array(
+            'name' => 'id',
+            'options' => array(
+                'label' => ''
+            ),
+            'attributes' => array(
+                'type' => 'hidden'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'text',
+            'options' => array(
+                'label' => 'Message'
+            ),
+            'attributes' => array(
+                'type' => 'textarea',
+                'class' => 'form-control',
+                'rows' => '6'
+            )
+        )
+        );
+
+        // Submit button.
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Post',
+                'id'    => 'submitbutton',
+                'class' => 'btn btn-primary'
+            ),
+        ));
+    }
+}
---
+++ tests/Zf2ForumTest/View/Helper/SmartTimeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Zf2ForumTest\View\Helper;
+
+use Zf2Forum\View\Helper\SmartTime as ViewHelper;
+
+/**
+ * Zf2Forum\View\Helper\SmartTime test case.
+ */
+class SmartTimeTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp();
+
+
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+
+
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+		// TODO Auto-generated constructor
+	}
+}
+
